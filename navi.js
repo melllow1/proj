41507cb143278b2e89a5ff1e9e101f38c0df227b
melllow1 <112881904+melllow1@users.noreply.github.com>
2024-05-02 05:23:50 +0300

    navi.js

--- navi.js
+++ navi.js
@@ -1,19 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
-    
-    const navLinks = document.querySelectorAll('.navigation a');
+    const themeToggler = document.getElementById('theme-toggler');
+    const body = document.body;
 
+    themeToggler.addEventListener('click', function() {
+        
+        if (body.classList.contains('light-theme')) {
+            body.classList.toggle('dark-mode');
+        } else if (body.classList.contains('dark-theme')) {
+            body.classList.toggle('light-green-theme');
+        }
+    });
+
+    const navLinks = document.querySelectorAll('.navigation a');
     navLinks.forEach(link => {
         link.addEventListener('click', function(event) {
-            // Prevent default anchor click behavior
             event.preventDefault();
-
             const destination = this.getAttribute('href');
-
             if (destination !== "#" && destination.trim() !== "") {
-                // Redirect to the link
                 window.location.href = destination;
             } else {
-                // Handle cases where no destination is provided
                 console.log("No destination provided for", this.textContent);
             }
         });
